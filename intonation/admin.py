1d81c1454322c27051de31de9d379ccc31eddf9f
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2018-01-30 09:45:57 -0500

    Update intonation admin

--- intonation/admin.py
+++ intonation/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from . import models
+
+
+class AnnotationCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class ItemTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+class AnnotationCategoryValueAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.AnnotationCategory, AnnotationCategoryAdmin)
+admin.site.register(models.AnnotationCategoryValue, AnnotationCategoryValueAdmin)
+admin.site.register(models.ItemType, ItemTypeAdmin)
