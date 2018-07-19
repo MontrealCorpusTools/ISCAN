c3fddd987511d3127c0ac35cacfbc900fbebcbdb
Michael Goodale <michael.goodale@mail.mcgill.ca>
2018-07-19 10:55:22 -0400

    cleaned indentation levels

--- pgdb/static/pgdb/enrichment/annotation-level/annotation-level.js
+++ pgdb/static/pgdb/enrichment/annotation-level/annotation-level.js
@@ -2,43 +2,42 @@ angular.module('annotationLevel', [
     'pgdb.corpora',
     'pgdb.enrichment'
 ]).controller('AnnotationLevelCtrl', function ($scope, $rootScope, Enrichment, Corpora, $state, $stateParams) {
-    $scope.annotation_options = [{name: "Utterances",
-                                  type: "utterances"},
-                                 {name: "Pauses",
-                                  type: "pauses"}];
-    $scope.enrichment = {};
-    $scope.count = 25;
+	$scope.annotation_options = [{name: "Utterances",
+				  type: "utterances"},
+				 {name: "Pauses",
+				  type: "pauses"}];
+	$scope.enrichment = {};
+	$scope.count = 25;
 
-    $scope.getWords = function(){
-	    Corpora.words($stateParams.corpus_id, $scope.count).then(function(res){
-		    $scope.words = JSON.parse(res.data);
-	    }).catch(function(res){
-		    $scope.error_message = res.data
-	    });
-    };
+	$scope.getWords = function(){
+		Corpora.words($stateParams.corpus_id, $scope.count).then(function(res){
+			$scope.words = JSON.parse(res.data);
+		}).catch(function(res){
+			$scope.error_message = res.data
+		});
+	};
 
-    $scope.getWords();
+	$scope.getWords();
 
-    $scope.getCheckedWords = function(){
+	$scope.getCheckedWords = function(){
 	    var arr = [];
 	    for (var i = 0; i < $scope.words.length; i++) {   
 		    if($scope.words[i].checked){
 			    arr.push($scope.words[i].label);
 		    }
 	    }
-	    console.log(arr);
 	    return arr;
-    };
+	};
 
-    $scope.save = function(){
-	$scope.enrichment.pause_length = $scope.enrichment.pause_length/1000;
-	if($scope.enrichment.enrichment_type == "pauses"){
-		$scope.enrichment.pause_label = $scope.getCheckedWords();
-	}
-        Enrichment.create($stateParams.corpus_id, $scope.enrichment).then(function (res){
-            $state.go('enrichment', {corpus_id: $stateParams.corpus_id});
-        }).catch(function(res){
-            $scope.error_message = res.data;
-        });
-    };
+	$scope.save = function(){
+		$scope.enrichment.pause_length = $scope.enrichment.pause_length/1000;
+		if($scope.enrichment.enrichment_type == "pauses"){
+			$scope.enrichment.pause_label = $scope.getCheckedWords();
+		}
+		Enrichment.create($stateParams.corpus_id, $scope.enrichment).then(function (res){
+			$state.go('enrichment', {corpus_id: $stateParams.corpus_id});
+		}).catch(function(res){
+			$scope.error_message = res.data;
+		});
+	};
 });
