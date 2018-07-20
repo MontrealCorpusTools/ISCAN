2d5f11dc35aeaa3cb0663e08f213f9d1c7a4f9ca
Michael Goodale <michaelericgoodale@gmail.com>
2018-07-20 14:41:51 -0400

    Added refined formant enrichment parameters (#24)
    Resolves #16

--- pgdb/static/pgdb/enrichment/acoustics/acoustics.js
+++ pgdb/static/pgdb/enrichment/acoustics/acoustics.js
@@ -31,8 +31,8 @@ angular.module('acoustics', [
     $scope.binary_options = ['praat'];
     $scope.phone_class = [{name:'test', type: 'test'}];
     $scope.enrichment = {source: 'praat'};
-    $scope.uploadPraatScript = function(id){
-	    var f = document.getElementById('praat-script-file').files[0],
+    $scope.uploadFile = function(id, file_id){
+	    var f = document.getElementById(file_id).files[0],
 	    r = new FileReader();
 	    var name = f.name;
 	    r.onloadend = function(e) {
@@ -45,7 +45,11 @@ angular.module('acoustics', [
     $scope.save = function () {
         Enrichment.create($stateParams.corpus_id, $scope.enrichment).then(function (res){
 	    if($scope.enrichment.enrichment_type == 'praat_script'){
-		    $scope.uploadPraatScript(res.data.id);
+		    $scope.uploadFile(res.data.id, "praat-script-file");
+	    }else if($scope.enrichment.enrichment_type == 'refined_formant_points'){
+		    if(document.getElementById(file_id).files.length > 0){
+			    $scope.uploadFile(res.data.id, "formants-file");
+		    }
 	    }
             $state.go('enrichment', {corpus_id: $stateParams.corpus_id});
         }).catch(function(res){
