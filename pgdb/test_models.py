6d145884d11c6c5abccc90cf22771a381461902c
Michael Goodale <michael.goodale@mail.mcgill.ca>
2018-08-06 10:54:01 -0400

    simplified database test set up

--- pgdb/test_models.py
+++ pgdb/test_models.py
@@ -9,41 +9,9 @@ from rest_framework import status
 
 class DatabaseTest(TestCase):
     def setUp(self):
-        used_ports = get_used_ports()
-        current_ports = []
-        data_dict = {'name': 'new_database',
-                     'neo4j_http_port': 7404,
-                     'neo4j_https_port': None,
-                     'neo4j_bolt_port': None,
-                     'neo4j_admin_port': None,   
-                     'influxdb_http_port': 8404,
-                     'influxdb_meta_port': None,
-                     'influxdb_udp_port': 8406,
-                     'influxdb_admin_port': None}
-        ports = {'neo4j': settings.BASE_NEO4J_PORT, 'influxdb': settings.BASE_INFLUXDB_PORT}
-        for k, v in data_dict.items():
-            if 'port' not in k:
-                continue
-            if v is None:
-                if 'neo4j' in k:
-                    port_key = 'neo4j'
-                else:
-                    port_key = 'influxdb'
-                while True:
-                    if ports[port_key] not in used_ports and ports[port_key] not in current_ports:
-                        data_dict[k] = ports[port_key]
-                        current_ports.append(ports[port_key])
-                        ports[port_key] += 1
-                        break
-                    ports[port_key] += 1
-        serializer = serializers.DatabaseSerializer(data=data_dict)
-        print(data_dict)
-        assert serializer.is_valid()
-        self.database = serializer.save()
-        self.database.install()
-        #self.database = Database.objects.create
+        self.database = Database.objects.create(
+        name='new_database', neo4j_http_port=7404, neo4j_https_port=7400, neo4j_bolt_port=7401, neo4j_admin_port=7402, influxdb_http_port=8404, influxdb_meta_port=8400, influxdb_udp_port=8406, influxdb_admin_port=8401);
         self.database.start()
-        time.sleep(1)
 
     def tearDown(self):
         self.database.delete()
@@ -65,37 +33,8 @@ class DatabaseTest(TestCase):
     
 class CorpusTest(TestCase):
     def setUp(self):
-        used_ports = get_used_ports()
-        current_ports = []
-        data_dict = {'name': 'new_database',
-                     'neo4j_http_port': 7404,
-                     'neo4j_https_port': None,
-                     'neo4j_bolt_port': None,
-                     'neo4j_admin_port': None,   
-                     'influxdb_http_port': 8404,
-                     'influxdb_meta_port': None,
-                     'influxdb_udp_port': 8406,
-                     'influxdb_admin_port': None}
-        ports = {'neo4j': settings.BASE_NEO4J_PORT, 'influxdb': settings.BASE_INFLUXDB_PORT}
-        for k, v in data_dict.items():
-            if 'port' not in k:
-                continue
-            if v is None:
-                if 'neo4j' in k:
-                    port_key = 'neo4j'
-                else:
-                    port_key = 'influxdb'
-                while True:
-                    if ports[port_key] not in used_ports and ports[port_key] not in current_ports:
-                        data_dict[k] = ports[port_key]
-                        current_ports.append(ports[port_key])
-                        ports[port_key] += 1
-                        break
-                    ports[port_key] += 1
-        serializer = serializers.DatabaseSerializer(data=data_dict)
-        assert serializer.is_valid()
-        self.database = serializer.save()
-        self.database.install()
+        self.database = Database.objects.create(
+        name='new_database', neo4j_http_port=7404, neo4j_https_port=7400, neo4j_bolt_port=7401, neo4j_admin_port=7402, influxdb_http_port=8404, influxdb_meta_port=8400, influxdb_udp_port=8406, influxdb_admin_port=8401);
         self.database.start()
         self.corpus = Corpus.objects.create(name="acoustic", database=self.database)
         print(self.corpus.name)
