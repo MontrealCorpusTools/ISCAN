5a9f35e67e3df3a1579bb19c8bbb7d0909491156
Michael Goodale <michael.goodale@mail.mcgill.ca>
2018-08-15 11:19:08 -0400

    Added travis config for docker, should fix #27

--- docker-utils/start.sh
+++ docker-utils/start.sh
@@ -20,6 +20,12 @@ else
 	    ${SITE_DIR}/env/bin/python manage.py test pgdb.test_web | true
 	    sleep 60
 	    ${SITE_DIR}/env/bin/python manage.py test pgdb.test_web
+	elif [ "$1" == 'test_cov' ]; then
+	    #Have to run twice in order to get the database list to refresh for some reason
+	    # | true is because set -eox pipfall exits on a non 0 exit code, and failed tests don't have 0 exit codes
+	    ${SITE_DIR}/env/bin/python manage.py test pgdb.test_web | true
+	    sleep 60
+	    coverage run --source='.' manage.py test pgdb.test_web
 	else
 	    exec "$@"
 	fi
