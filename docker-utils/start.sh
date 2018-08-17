7834c6725b65eddde4bb20c16b430abfa74941ee
Michael Goodale <michael.goodale@mail.mcgill.ca>
2018-08-17 11:13:31 -0400

    Removed unnecessary tricks

--- docker-utils/start.sh
+++ docker-utils/start.sh
@@ -15,16 +15,8 @@ else
 	    echo "Manage.py $@"
 	    ${SITE_DIR}/env/bin/python ${SITE_DIR}/proj/manage.py $@
 	elif [ "$1" == 'test' ]; then
-	    #Have to run twice in order to get the database list to refresh for some reason
-	    # | true is because set -eox pipfall exits on a non 0 exit code, and failed tests don't have 0 exit codes
-	    #${SITE_DIR}/env/bin/python manage.py test pgdb.test_web | true
-	    #sleep 60
 	    ${SITE_DIR}/env/bin/python manage.py test pgdb.test_web
 	elif [ "$1" == 'test_cov' ]; then
-	    #Have to run twice in order to get the database list to refresh for some reason
-	    # | true is because set -eox pipfall exits on a non 0 exit code, and failed tests don't have 0 exit codes
-	    ${SITE_DIR}/env/bin/python manage.py test pgdb.test_web | true
-	    sleep 60
 	    coverage run --source='.' manage.py test pgdb.test_web
 	else
 	    exec "$@"
