beac25358dee5f0f43cdbbcba384b32485180117
Michael Goodale <michael.goodale@mail.mcgill.ca>
2019-01-07 18:33:23 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def get_version(package):
     return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 
-version = get_version('iscan')
+version = get_version('pgdb')
 
 class PyTest(TestCommand):
     def finalize_options(self):
