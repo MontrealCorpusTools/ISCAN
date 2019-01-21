26126e2d7403a7ff2a7a3027564cf9d94176451f
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2019-01-21 09:46:12 -0500

    Update hostname

--- iscan/static/iscan/env.js
+++ iscan/static/iscan/env.js
@@ -1,6 +1,6 @@
 (function (window) {
   window.__env = window.__env || {};
-  window.__env.hostUrl = 'http://localhost:8080/';
+  window.__env.hostUrl = 'https://' + window.location.hostname +'/';
   window.__env.apiUrl = window.__env.hostUrl+'api/';
   window.__env.intontationUrl = window.__env.hostUrl +'intonation/';
   window.__env.annotatorUrl = window.__env.hostUrl +'annotator/api/';
