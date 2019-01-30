614b4d791cf3660f1e84550cc17e86c2d10f5c9f
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2019-01-30 10:03:39 -0500

    Fix for missing port on API requests

--- iscan/static/iscan/env.js
+++ iscan/static/iscan/env.js
@@ -1,10 +1,15 @@
 (function (window) {
-  window.__env = window.__env || {};
-  window.__env.hostUrl = 'https://' + window.location.hostname +'/';
-  window.__env.apiUrl = window.__env.hostUrl+'api/';
-  window.__env.intontationUrl = window.__env.hostUrl +'intonation/';
-  window.__env.annotatorUrl = window.__env.hostUrl +'annotator/api/';
-  window.__env.baseUrl = '/';
-  window.__env.siteName = 'ISCAN';
-  window.__env.enableDebug = true;
+    window.__env = window.__env || {};
+    if (window.location.port) {
+        window.__env.hostUrl = 'https://' + window.location.hostname + ':' + window.location.port + '/';
+    }
+    else {
+        window.__env.hostUrl = 'https://' + window.location.hostname + '/';
+    }
+    window.__env.apiUrl = window.__env.hostUrl + 'api/';
+    window.__env.intontationUrl = window.__env.hostUrl + 'intonation/';
+    window.__env.annotatorUrl = window.__env.hostUrl + 'annotator/api/';
+    window.__env.baseUrl = '/';
+    window.__env.siteName = 'ISCAN';
+    window.__env.enableDebug = true;
 }(this));
