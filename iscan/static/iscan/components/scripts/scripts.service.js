814482928b00f84a848aacb39635f5ff03dd0f79
Michael Goodale <michael.goodale@mail.mcgill.ca>
2019-08-12 16:19:35 -0400

    Fixed page layout and API js

--- iscan/static/iscan/components/scripts/scripts.service.js
+++ iscan/static/iscan/components/scripts/scripts.service.js
@@ -9,13 +9,13 @@ angular.module('iscan.scripts')
         return $http.post(base_url+'run_script/', {});
     };
 
-    Enrichment.list = function () {
+    Scripts.list = function () {
         return $http.get(base_url);
     };
 
-    Enrichment.list_corpora = function () {
+    Scripts.list_corpora = function () {
         return $http.get(base_url + 'list_corpora/');
     };
 
-    return Errors;
+    return Scripts;
 });
